d86aa8d6b873ead732a1d93990230bd15524c3a6
Lucien Akchoté <lakchote@icloud.com>
2022-11-26 00:38:54 +0100

    Featured collections mock

--- components/FeaturedCollection/index.tsx
+++ components/FeaturedCollection/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { featuredCollections } from "../../constants/mocks/featuredCollections"
 
 export default function FeaturedCollection() {
   return (
@@ -7,96 +8,40 @@ export default function FeaturedCollection() {
         Collection featured NFTs
       </h2>
       <div className="lg:inline-flex lg:ml-[144px] lg:space-x-8">
-        <div className="flex flex-col ml-4 lg:ml-0">
-          <div className="lg:pt-14 flex">
-            <Image
-              src="/nft_collections/featured/1_main.png"
-              alt=""
-              width={266}
-              height={272}
-              className="float-left mr-[10px]"
-            />
-            <div className="space-y-2 mr-1 lg:mr-0">
-              <Image src="/nft_collections/featured/1_y_0.png" alt="" width={103} height={85} />
-              <Image src="/nft_collections/featured/1_y_1.png" alt="" width={103} height={85} />
-              <Image src="/nft_collections/featured/1_y_2.png" alt="" width={103} height={85} />
-            </div>
-          </div>
-          <div className="flex-col">
-            <div className="font-dmSansBold tracking-wide text-xl pt-7">Amazing collection</div>
-            <div className="flex flex-row items-center -mt-8 justify-between lg:w-[379px]">
-              <div className="items-center inline-flex">
-                <Image src="/user_gradient.png" alt="" width={32} height={32} className="mr-[14px]" />
-                <span className="text-secondary text-sm font-dmSansRegular">by Arkhan</span>
+        {featuredCollections.map((collection, i) => {
+          return (
+            <div className="flex flex-col ml-4 lg:ml-0" key={i}>
+              <div className="lg:pt-14 flex">
+                <Image
+                  src={collection.images.main}
+                  alt=""
+                  width={266}
+                  height={272}
+                  className="float-left mr-[10px] lg:max-h-[272px]"
+                />
+                <div className="space-y-2 mr-1 lg:mr-0">
+                  <Image src={collection.images.side_y_0} alt="" width={103} height={85} />
+                  <Image src={collection.images.side_y_1} alt="" width={103} height={85} />
+                  <Image src={collection.images.side_y_2} alt="" width={103} height={85} />
+                </div>
               </div>
-              <div className="mr-1 lg:mr-0">
-                <button className="my-10 border-2 border-accent text-[11px] text-accent font-dmSansBold rounded-[60px] py-1 px-2">
-                  Total 54 items
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col ml-4 lg:ml-0">
-          <div className="lg:pt-14 flex">
-            <Image
-              src="/nft_collections/featured/1_main.png"
-              alt=""
-              width={266}
-              height={272}
-              className="float-left mr-[10px]"
-            />
-            <div className="space-y-2 mr-1 lg:mr-0">
-              <Image src="/nft_collections/featured/1_y_0.png" alt="" width={103} height={85} />
-              <Image src="/nft_collections/featured/1_y_1.png" alt="" width={103} height={85} />
-              <Image src="/nft_collections/featured/1_y_2.png" alt="" width={103} height={85} />
-            </div>
-          </div>
-          <div className="flex-col">
-            <div className="font-dmSansBold tracking-wide text-xl pt-7">Amazing collection</div>
-            <div className="flex flex-row items-center -mt-8 justify-between lg:w-[379px]">
-              <div className="items-center inline-flex">
-                <Image src="/user_gradient.png" alt="" width={32} height={32} className="mr-[14px]" />
-                <span className="text-secondary text-sm font-dmSansRegular">by Arkhan</span>
-              </div>
-              <div className="mr-1 lg:mr-0">
-                <button className="my-10 border-2 border-accent text-[11px] text-accent font-dmSansBold rounded-[60px] py-1 px-2">
-                  Total 54 items
-                </button>
+              <div className="flex-col">
+                <div className="font-dmSansBold tracking-wide text-xl pt-7">Amazing collection</div>
+                <div className="flex flex-row items-center -mt-8 justify-between lg:w-[379px]">
+                  <div className="items-center inline-flex">
+                    <Image src="/user_gradient.png" alt="" width={32} height={32} className="mr-[14px]" />
+                    <span className="text-secondary text-sm font-dmSansRegular">by Arkhan</span>
+                  </div>
+                  <div className="mr-1 lg:mr-0">
+                    <button className="my-10 border-2 border-accent text-[11px] text-accent font-dmSansBold rounded-[60px] py-1 px-2">
+                      Total 54 items
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="flex flex-col ml-4 lg:ml-0">
-          <div className="lg:pt-14 flex">
-            <Image
-              src="/nft_collections/featured/1_main.png"
-              alt=""
-              width={266}
-              height={272}
-              className="float-left mr-[10px]"
-            />
-            <div className="space-y-2 mr-1 lg:mr-0">
-              <Image src="/nft_collections/featured/1_y_0.png" alt="" width={103} height={85} />
-              <Image src="/nft_collections/featured/1_y_1.png" alt="" width={103} height={85} />
-              <Image src="/nft_collections/featured/1_y_2.png" alt="" width={103} height={85} />
-            </div>
-          </div>
-          <div className="flex-col">
-            <div className="font-dmSansBold tracking-wide text-xl pt-7">Amazing collection</div>
-            <div className="flex flex-row items-center -mt-8 justify-between lg:w-[379px]">
-              <div className="items-center inline-flex">
-                <Image src="/user_gradient.png" alt="" width={32} height={32} className="mr-[14px]" />
-                <span className="text-secondary text-sm font-dmSansRegular">by Arkhan</span>
-              </div>
-              <div className="mr-1 lg:mr-0">
-                <button className="my-10 border-2 border-accent text-[11px] text-accent font-dmSansBold rounded-[60px] py-1 px-2">
-                  Total 54 items
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+          )
+        })}
       </div>
     </div>
   )
---
+++ constants/mocks/featuredCollections.ts
@@ -0,0 +1,26 @@
+export const featuredCollections = [
+  {
+    images: {
+      main: "/nft_collections/featured/1_main.png",
+      side_y_0: "/nft_collections/featured/1_y_0.png",
+      side_y_1: "/nft_collections/featured/1_y_1.png",
+      side_y_2: "/nft_collections/featured/1_y_2.png",
+    },
+  },
+  {
+    images: {
+      main: "/stacked_img_1.png",
+      side_y_0: "/nft_collections/featured/1_y_1.png",
+      side_y_1: "/nft_collections/featured/1_y_0.png",
+      side_y_2: "/nft_collections/featured/2_y_2.png",
+    },
+  },
+  {
+    images: {
+      main: "/nft_collections/featured/3_main.png",
+      side_y_0: "/nft_collections/featured/3_y_0.png",
+      side_y_1: "/nft_collections/featured/3_y_1.png",
+      side_y_2: "/nft_collections/featured/1_y_0.png",
+    },
+  },
+]
